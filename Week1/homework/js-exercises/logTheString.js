b4b5cdfc5105796a6ee8cf71a6492896c0b06c00
obada othman <obada.oth@gmail.com>
2020-07-26 09:15:11 +0200

    Added exercise 4 , which I forgot to add originally

---
+++ Week1/homework/js-exercises/logTheString.js
@@ -0,0 +1,6 @@
+let myString = "Obada Othman";
+console.log('The value of myString is "Obada Othman"');
+console.log(myString);
+myString = "Namhto Adabo";
+console.log('The value of myString now is "Namhto Adabo"');
+console.log(myString);
