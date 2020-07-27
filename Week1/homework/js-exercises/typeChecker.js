e52de2e0eef7824a3098999264a63de83c462511
obada othman <obada.oth@gmail.com>
2020-07-27 18:17:21 +0200

    made changes requested by reviewer on type-checker.js & round.js

--- Week1/homework/js-exercises/typeChecker.js
+++ Week1/homework/js-exercises/typeChecker.js
@@ -1,28 +1,22 @@
-'use strict'
-var a="string1";
-var b="string2";
-var c= {};
-var d= {};
-function typeComparison(arg1, arg2){
-    let type1=typeof arg1;
-    let type2=typeof arg2;
-    let result;
-    if (type1===type2){
-       return result="Same type"
-    }
-    else {
-        return result="Not the same"
-    }
-   return result;
+"use strict";
+var a = "string1";
+var b = "string2";
+var c = {};
+var d = {};
+function typeComparison(arg1, arg2) {
+  let type1 = typeof arg1;
+  let type2 = typeof arg2;
+  let result;
+  if (type1 === type2) {
+    return "Same type";
+  } else {
+    return "Not the same";
+  }
 }
 
-console.log("a & b are "+typeComparison(a, b));
-console.log("a & c are "+typeComparison(a, c));
-console.log("a & d are "+typeComparison(a, d));
-console.log("b & c are "+typeComparison(b, c));
-console.log("b & d are "+typeComparison(b, d));
-console.log("c & d are "+typeComparison(c, d));
-
-
-
-
+console.log("a & b are " + typeComparison(a, b));
+console.log("a & c are " + typeComparison(a, c));
+console.log("a & d are " + typeComparison(a, d));
+console.log("b & c are " + typeComparison(b, c));
+console.log("b & d are " + typeComparison(b, d));
+console.log("c & d are " + typeComparison(c, d));
