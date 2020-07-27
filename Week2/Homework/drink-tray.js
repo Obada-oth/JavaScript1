b30167f8c0756ca05632ffde247005d60db06155
obada othman <obada.oth@gmail.com>
2020-07-27 19:07:59 +0200

    made some changes on drink-tray.js (not finished)

--- Week2/Homework/drink-tray.js
+++ Week2/Homework/drink-tray.js
@@ -1,12 +1,1 @@
 //pushing array's element into another array
-const drinkTypes = ["cola", "lemonade", "water"];
-const drinkTray = [];
-const instances = {};
-function addToTray(){
-for (let i = 0; i <= 5; i++) {
-
-  instances[drinkTray[i]] = (instances[drinkTray[i]] || 0) + 1;
-  if (instances[drinkTray[i]])
-}
-
-console.log(instances[drinkTray[i]]);
