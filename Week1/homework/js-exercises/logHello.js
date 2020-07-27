b9c67f0aee4350828d4330ddada81dc76d87a19d
obada othman <obada.oth@gmail.com>
2020-07-27 18:27:37 +0200

    made changes requested by reviewer on logHello.js

--- Week1/homework/js-exercises/logHello.js
+++ Week1/homework/js-exercises/logHello.js
@@ -1,14 +1,14 @@
-'use strict'
-let arrLanguages= [
-    "Hallo wereld",// Dutch
-    "你好世界",//Chinese
-    "Halo, dunia!",//Indonesian
-    "Ciao, mondo!",//Italian
-    "Hola, mundo!",//Spanish
-    "Bonjour monde",//French
-    "γειά σου κόσμος",//Greek
-    "Hallo Welt",//German
-    "Olá mundo",//Portoguese
-    "Здравствуй, мир",//Russian
+"use strict";
+let arrLanguages = [
+  "Hallo wereld", // Dutch
+  "你好世界", //Chinese
+  "Halo, dunia!", //Indonesian
+  "Ciao, mondo!", //Italian
+  "Hola, mundo!", //Spanish
+  "Bonjour monde", //French
+  "γειά σου κόσμος", //Greek
+  "Hallo Welt", //German
+  "Olá mundo", //Portoguese
+  "Здравствуй, мир", //Russian
 ];
-console.log(arrLanguages);
+arrLanguages.forEach((element) => console.log(element));
