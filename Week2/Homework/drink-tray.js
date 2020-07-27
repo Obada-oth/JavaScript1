28b2f57dbb6fcb7baa3e73d27f84007fdc894e82
obada othman <obada.oth@gmail.com>
2020-07-27 18:08:35 +0200

    made changes requested by reviewer on grad-calculator.js &

--- Week2/Homework/drink-tray.js
+++ Week2/Homework/drink-tray.js
@@ -1,14 +1,12 @@
 //pushing array's element into another array
-"use strict";
 const drinkTypes = ["cola", "lemonade", "water"];
-let drinkTray = [];
-for (let i = 0; i < 5; i++) {
-  if (drinkTray.length <= 1) {
-    drinkTray.push(drinkTypes[0]);
-  } else if (drinkTray.length >= 2 && drinkTray.length <= 3) {
-    drinkTray.push(drinkTypes[1]);
-  } else {
-    drinkTray.push(drinkTypes[2]);
-  }
-  console.log("Hey guys! I brought " + drinkTray);
+const drinkTray = [];
+const instances = {};
+function addToTray(){
+for (let i = 0; i <= 5; i++) {
+
+  instances[drinkTray[i]] = (instances[drinkTray[i]] || 0) + 1;
+  if (instances[drinkTray[i]])
 }
+
+console.log(instances[drinkTray[i]]);
--- Week2/Homework/grade-calculator.js
+++ Week2/Homework/grade-calculator.js
@@ -2,7 +2,7 @@
 function gradeCalculator(score) {
   let percentage =
     "(" +
-    (Math.round((score / 100) * 100) + "%") +
+    (Math.floor((score / 100) * 100) + "%") +
     ")"; /*declaring the percentage variable and calculating it based on score */
   switch (true) {
     case score >= 90 &&
@@ -10,22 +10,22 @@ function gradeCalculator(score) {
         100 /* evaluates the score against different ranges and returns a grade*/:
       return (grade = "A " + percentage);
 
-    case score >= 80 && score <= 89:
+    case score >= 80 && score < 90:
       return (grade = "B " + percentage);
-    case score >= 70 && score <= 79:
+    case score >= 70 && score < 80:
       return (grade = "C " + percentage);
 
-    case score >= 60 && score <= 69:
+    case score >= 60 && score < 70:
       return (grade = "D " + percentage);
 
-    case score >= 50 && score <= 59:
+    case score >= 50 && score < 60:
       return (grade = "E " + percentage);
 
-    case score >= 0 && score <= 49:
+    case score >= 0 && score < 50:
       return (grade = "F " + percentage);
   }
 }
 
 console.log(
-  `You got a ${gradeCalculator(54)}!`
+  `You got a ${gradeCalculator(49.6)}!`
 ); /*logs a template string that includes the grade and the percentage*/
