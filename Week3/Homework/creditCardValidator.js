a9f4a41e5a3b460cdc6758909a0647463d823b89
obada othman <obada.oth@gmail.com>
2020-07-29 22:21:12 +0200

    Finished homework for week3 of JS-1

--- Week3/Homework/creditCardValidator.js
+++ Week3/Homework/creditCardValidator.js
@@ -2,6 +2,8 @@
 function validateCreditCard(num) {
   let summedDigits = 0;
   let stringNum = num.toString();
+  let frequency = stringNum.split(stringNum[0]).length - 1;
+
   for (let i = 0; i < stringNum.length; i++) {
     summedDigits += parseInt(stringNum.charAt(i));
   }
@@ -10,12 +12,21 @@ function validateCreditCard(num) {
     !isNaN(num) &&
     stringNum.length == 16 &&
     summedDigits > 16 &&
-    (num % 10) % 2 == 0
+    (num % 10) % 2 == 0 &&
+    frequency < 16
   ) {
-    return "success!";
-  } else {
-    return "invalidoo";
+    return `success! The input ${num} is a valid credit card number`;
+  } else if (isNaN(num)) {
+    return `Invalid! The input ${num} must contain only numbers`;
+  } else if (stringNum.length !== 16) {
+    return `Invalid! The input ${num} must be 16 digits long`;
+  } else if (summedDigits <= 16) {
+    return `Invalid! The sum of all digits in ${num} must be greater than 16`;
+  } else if ((num % 10) % 2 !== 0) {
+    return `Invalid! The last digit in ${num} must be an even number. `;
+  } else if (frequency >= 16) {
+    return `Invalid! The input ${num} must contain at least two different numbers`;
   }
 }
 
-console.log(validateCreditCard("1111111111166001"));
+console.log(validateCreditCard("2222222222222224"));
--- Week3/Homework/dog-years.js
+++ Week3/Homework/dog-years.js
@@ -3,15 +3,9 @@
 function calculateDogAge(ageInHumanYears) {
   /*This function returns dog's age based on human years */
   let ageInDogYears = ageInHumanYears * 7;
-  return ageInDogYears;
+  return `Your doggie is ${ageInDogYears} years old in dog years`;
 }
 
-console.log(
-  `Your doggie is ${calculateDogAge(1)} years old in dog years!`
-); /*Your doggie is 7 years old in dog years! */
-console.log(
-  `Your doggie is ${calculateDogAge(2)} years old in dog years!`
-); /*Your doggie is 14 years old in dog years! */
-console.log(
-  `Your doggie is ${calculateDogAge(3)} years old in dog years!`
-); /*Your doggie is 21 years old in dog years! */
+console.log(calculateDogAge(1));
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(7));
