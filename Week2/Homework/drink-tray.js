e9cc6098b3914ff4db721a6eec3f6f1009be3c6c
obada othman <obada.oth@gmail.com>
2020-07-29 10:16:52 +0200

    made changes requested by reviewer on drink-tray.js

--- Week2/Homework/drink-tray.js
+++ Week2/Homework/drink-tray.js
@@ -1,1 +1,19 @@
 //pushing array's element into another array
+"use strict";
+const drinkTypes = ["cola", "lemonad", "water"];
+const drinkTray = [];
+const count = [0, 0, 0]; /*counts the instances fro each drink type */
+let j = 0;
+
+for (let i = 0; i < 5; i++) {
+  drinkTray.push(drinkTypes[j]);
+  count[j]++; /*push a drink type , then increas the count for that type by 1 */
+
+  if (count[j] == 2) {
+    /*When the count is equal to 2 start adding the next drink type */
+    j++;
+  }
+  console.log(count);
+}
+
+console.log(drinkTray);
